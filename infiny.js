7286ede0248e575ea8dca604f7134eadb1881ba1
Adithya S Narasinghe <adithyasnarasinghe@gmail.com>
2021-06-09 14:26:11 +0530

    Update to support WhatsApp Web Version 2.2121.6

--- infiny.js
+++ infiny.js
@@ -2,41 +2,10 @@ var jq = document.createElement('script');
 jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
 document.getElementsByTagName('head')[0].appendChild(jq);
 
-contacts = [];
-chat_div = [];
-
-function triggerMouseEvent(node, eventType) {
-    var event = document.createEvent('MouseEvents');
-    event.initEvent(eventType, true, true);
-    node.dispatchEvent(event);
-}
-
-function getChatname(){
-    $("#pane-side > div > div > div").find("._25Ooe > ._1wjpf").each(function(){
-        console.log($(this).text())
-        contacts.push($(this).text());
-        chat_div.push($(this));
-    })
-    
-     console.log($("#pane-side > div > div > div"))
-}
-
-function selectContact(name){
-    getChatname()
-    for (i = 0; i < contacts.length; i++){
-        if (name.toUpperCase() === contacts[i].toUpperCase()){
-            console.log(name.toUpperCase() + " match with "+contacts[i].toUpperCase())
-             triggerMouseEvent(chat_div[i][0],"mousedown")
-        }else{
-            console.log(name.toUpperCase() + " mismatch with "+contacts[i].toUpperCase())
-        }
-    }
-}
-
 function sendMessage(text){
-    $("#main > footer > div._3SvgF > div.DuUXI > div > div._1awRl.copyable-text.selectable-text").text(text)
-    input = document.querySelector("#main > footer > div._3SvgF > div.DuUXI > div > div._1awRl.copyable-text.selectable-text");
+    $("#main > footer > div.copyable-area > div._2A8P4 > div > div.copyable-text.selectable-text").text(text)
+    input = document.querySelector("#main > footer > div.copyable-area > div._2A8P4 > div > div.copyable-text.selectable-text");
     input.dispatchEvent(new Event('input', {bubbles: true}));
-	var button = document.querySelector('#main > footer > div._3SvgF > div._3qpzV > button');
-	button.click();
+    var button = document.querySelector('#main > footer > div.copyable-area > div.EBaI7 > button');
+    button.click();
 }
